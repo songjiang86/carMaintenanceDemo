6d11c0d6a70af8875d49c43db29e7716bd05636c
AlexYu87 <alexyu87@outlook.com>
2015-06-17 00:56:10 +0800

    Add angularAMD

--- script/js/application.js
+++ script/js/application.js
@@ -1,17 +1,17 @@
-define(["angularAMD", "angular-route", "application-route", "angular-require", "angularUI",
+define(["angularAMD", "angular-require", "angularUI",
 	"ui.bootstrap", "jquery"], 
 	function(angularAMD, angularRoute, appRouter){
-		var application = angular.module("car", ["ngRoute", "ng", "ngRequire", 
-			"ui.utils", "ui.bootstrap"]).config(["$routeProvider", function($routeProvider){
-				angular.forEach(appRouter, function(value){
-					$routeProvider.when(value[0], angularAMD.route(value[1]));	
-				});
-				
-				$routeProvider.otherwise({redirectTo: "/car"});	
-			}]);
+		var app = angular.module("car", ["ng", "ui.bootstrap"]);
+		app.config(function($routeProvider){
+			$routeProvider.when(
+				'home', 
+				angularAMD.route({
+					templateUrl: "home.html",
+					controller: "HomeController.js",
+					controllerUrl: 'script/js/controllers/HomeController.js'
+				})
+			);
+		})
 			
-			angular.resumeBootstrap = angular.noop;
-			angularAMD.bootstrap(application, document);
-			
-			return angularAMD;
+		return anngularAMD.bootstrap(app);	
 });
---
+++ script/js/controllers/HomeController.js
@@ -0,0 +1,3 @@
+define(['angularAMD'], function(angularAMD){
+	angularAMD
+});
